e2305c2beca9b445befcdba44ae309d15c73d0be
Gabby Losch <glosch00@gmail.com>
2015-06-05 10:14:27 -0400

    index page, menu, some css, animations

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,10 @@
 class HomeController < ApplicationController
   def index
-    # render json: "hi ohmygoditworked"
   end
 
   def f2fcall
-    request = HTTParty.get("https://food2fork.com/api/search?key=089c1ff7f1ed08d8c9b2f763dd260fe4")
-    render json: request
+    request = HTTParty.get("http://food2fork.com/api/search?key=089c1ff7f1ed08d8c9b2f763dd260fe4")
+    # binding.pry
+    render json: request.to_json
   end
 end
