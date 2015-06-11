18f48c5dc730195c5c622d02eee8476db9d7715a
Gabby Losch <glosch00@gmail.com>
2015-06-11 15:34:44 -0400

    commented out Surprise Me button until functionality added

--- app/assets/javascripts/backbone/views/RecipeView.js
+++ app/assets/javascripts/backbone/views/RecipeView.js
@@ -3,7 +3,7 @@ var Cookbook = Cookbook || { Models: {}, Collection: {}, Views: {} };
 Cookbook.Views.RecipeView = Backbone.View.extend({
   initialize: function(){
     console.log("recipe view initialized");
-    console.log(this.model + " is the model when initialized");
+    console.log(this.model + " is the model when initialized")
   },
 
   events: {
@@ -12,25 +12,21 @@ Cookbook.Views.RecipeView = Backbone.View.extend({
 
   saveModel: function(){
     console.log("RecipeView saveModel function triggered!");
-    var newRecipeModel = new Cookbook.Models.Recipe({
-      f2f_url: "http://www.google.com",
-      title: "Julia's potatoes",
-      recipe_id: 349872034,
-      image_url: "http://www.google.com",
-      social_rank: 100,
-      user_id: 1
-    });
-    newRecipeModel.save();
+    console.log(this.model);
+    this.model.save();
   },
 
-  el: $("#container"),
+  // el: $("#container"),
 
   template: $("#search-results").text(),
 
   render: function(){
     // console.log("RecipeView render triggered");
+    //var template = this.template
     var template = $("#search-results").text();
     var recipeCard = Mustache.render(template, {recipeCardBlock: this.model.attributes});
+    //this.$el.html(recipeCard);
+    //$("#container").append(this.el);
     this.$el.append(recipeCard);
   }
 
