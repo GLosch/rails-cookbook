368b79d2e3c0405b65688d542f7877d87103a009
Gabby Losch <glosch00@gmail.com>
2015-07-23 15:30:22 -0400

    homepage welcome message

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,139 +8,139 @@
 
 Recipe.delete_all
 
-Recipe.create({
-  user_id: 1,
-  f2f_url: "http://food2fork.com/view/46906",
-  title: "Roast Chicken",
-  recipe_id: "46906",
-  image_url: "http://static.food2fork.com/roastchicken2feab.jpg",
-  social_rank: 100
-})
+# Recipe.create({
+#   user_id: 1,
+#   f2f_url: "http://food2fork.com/view/46906",
+#   title: "Roast Chicken",
+#   recipe_id: "46906",
+#   image_url: "http://static.food2fork.com/roastchicken2feab.jpg",
+#   social_rank: 100
+# })
 
-Recipe.create({
-  user_id: 1,
-  f2f_url: "http://food2fork.com/view/47264",
-  title: "Ranch Style Chicken",
-  recipe_id: "47264",
-  image_url: "http://static.food2fork.com/3214830317_3470607769fad0.jpg",
-  social_rank: 99.99999999999457
-})
+# Recipe.create({
+#   user_id: 1,
+#   f2f_url: "http://food2fork.com/view/47264",
+#   title: "Ranch Style Chicken",
+#   recipe_id: "47264",
+#   image_url: "http://static.food2fork.com/3214830317_3470607769fad0.jpg",
+#   social_rank: 99.99999999999457
+# })
 
-Recipe.create({
-  user_id: 1,
-  f2f_url: "http://food2fork.com/view/39635",
-  title: "Slow-Cooker Chicken Tikka Masala",
-  recipe_id: "39635",
-  image_url: "http://static.food2fork.com/slowcookerchicken_300cae9237d.jpg",
-  social_rank: 99.99999998344464
-})
+# Recipe.create({
+#   user_id: 1,
+#   f2f_url: "http://food2fork.com/view/39635",
+#   title: "Slow-Cooker Chicken Tikka Masala",
+#   recipe_id: "39635",
+#   image_url: "http://static.food2fork.com/slowcookerchicken_300cae9237d.jpg",
+#   social_rank: 99.99999998344464
+# })
 
-Recipe.create({
-  user_id: 1,
-  f2f_url: "http://food2fork.com/view/47118",
-  title: "Grilled Chicken with Lemon Basil Pasta",
-  recipe_id: "47118",
-  image_url: "http://static.food2fork.com/4814287904_bb43e024c9_be8a9.jpg",
-  social_rank: 99.99999991847115
-})
+# Recipe.create({
+#   user_id: 1,
+#   f2f_url: "http://food2fork.com/view/47118",
+#   title: "Grilled Chicken with Lemon Basil Pasta",
+#   recipe_id: "47118",
+#   image_url: "http://static.food2fork.com/4814287904_bb43e024c9_be8a9.jpg",
+#   social_rank: 99.99999991847115
+# })
 
-Recipe.create({
-  user_id: 1,
-  f2f_url: "http://food2fork.com/view/54290",
-  title: "Lemon Chicken Orzo Soup",
-  recipe_id: "54290",
-  image_url: "http://static.food2fork.com/LemonChickenOrzoSoup33989.jpg",
-  social_rank: 99.99999952354868
-})
+# Recipe.create({
+#   user_id: 1,
+#   f2f_url: "http://food2fork.com/view/54290",
+#   title: "Lemon Chicken Orzo Soup",
+#   recipe_id: "54290",
+#   image_url: "http://static.food2fork.com/LemonChickenOrzoSoup33989.jpg",
+#   social_rank: 99.99999952354868
+# })
 
-Recipe.create({
-  user_id: 1,
-  f2f_url: "http://food2fork.com/view/49390",
-  title: "Parmesan Chicken with Caesar Roasted Romaine",
-  recipe_id: "49390",
-  image_url: "http://static.food2fork.com/parmesanchickenwithcaesarroastedromaine646e424.jpg",
-  social_rank: 99.99999950330772
-})
+# Recipe.create({
+#   user_id: 1,
+#   f2f_url: "http://food2fork.com/view/49390",
+#   title: "Parmesan Chicken with Caesar Roasted Romaine",
+#   recipe_id: "49390",
+#   image_url: "http://static.food2fork.com/parmesanchickenwithcaesarroastedromaine646e424.jpg",
+#   social_rank: 99.99999950330772
+# })
 
-Recipe.create({
-  user_id: 1,
-  f2f_url: "http://food2fork.com/view/35581",
-  title: "Slow Cooker Bacon Wrapped Apple BBQ Chicken",
-  recipe_id: "35581",
-  image_url: "http://static.food2fork.com/Slow2BCooker2BBacon2BWrapped2BApple2BBBQ2BChicken2B5002B4688989e0a75.jpg",
-  social_rank: 99.99999915732317
-})
+# Recipe.create({
+#   user_id: 1,
+#   f2f_url: "http://food2fork.com/view/35581",
+#   title: "Slow Cooker Bacon Wrapped Apple BBQ Chicken",
+#   recipe_id: "35581",
+#   image_url: "http://static.food2fork.com/Slow2BCooker2BBacon2BWrapped2BApple2BBBQ2BChicken2B5002B4688989e0a75.jpg",
+#   social_rank: 99.99999915732317
+# })
 
-Recipe.create({
-  user_id: 1,
-  f2f_url: "http://food2fork.com/view/47302",
-  title: "Crispy Yogurt Chicken",
-  recipe_id: "47302",
-  image_url: "http://static.food2fork.com/CrispyYogurtChickende81.jpg",
-  social_rank: 99.99999695511634
-})
+# Recipe.create({
+#   user_id: 1,
+#   f2f_url: "http://food2fork.com/view/47302",
+#   title: "Crispy Yogurt Chicken",
+#   recipe_id: "47302",
+#   image_url: "http://static.food2fork.com/CrispyYogurtChickende81.jpg",
+#   social_rank: 99.99999695511634
+# })
 
-Recipe.create({
-  user_id: 1,
-  f2f_url: "http://food2fork.com/view/a8bb2b",
-  title: "Foolproof slow roast chicken",
-  recipe_id: "a8bb2b",
-  image_url: "http://static.food2fork.com/4752_MEDIUMddd4.jpg",
-  social_rank: 99.99829699372306
-})
+# Recipe.create({
+#   user_id: 1,
+#   f2f_url: "http://food2fork.com/view/a8bb2b",
+#   title: "Foolproof slow roast chicken",
+#   recipe_id: "a8bb2b",
+#   image_url: "http://static.food2fork.com/4752_MEDIUMddd4.jpg",
+#   social_rank: 99.99829699372306
+# })
 
-Recipe.create({
-  user_id: 1,
-  f2f_url: "http://food2fork.com/view/0b1b8f",
-  title: "Emeril's Beef Stew",
-  recipe_id: "0b1b8f",
-  image_url: "http://static.food2fork.com/EE11171_EmerilsBeefStew_s4x3_lg1368.jpg",
-  social_rank: 99.1752424619898
-})
+# Recipe.create({
+#   user_id: 1,
+#   f2f_url: "http://food2fork.com/view/0b1b8f",
+#   title: "Emeril's Beef Stew",
+#   recipe_id: "0b1b8f",
+#   image_url: "http://static.food2fork.com/EE11171_EmerilsBeefStew_s4x3_lg1368.jpg",
+#   social_rank: 99.1752424619898
+# })
 
-Recipe.create({
-  user_id: 1,
-  f2f_url: "http://food2fork.com/view/e0b88e",
-  title: "Rosemary Beef with Root Vegetables",
-  recipe_id: "e0b88e",
-  image_url: "http://static.food2fork.com/51127010846e.jpg",
-  social_rank: 98.74066023551296
-})
+# Recipe.create({
+#   user_id: 1,
+#   f2f_url: "http://food2fork.com/view/e0b88e",
+#   title: "Rosemary Beef with Root Vegetables",
+#   recipe_id: "e0b88e",
+#   image_url: "http://static.food2fork.com/51127010846e.jpg",
+#   social_rank: 98.74066023551296
+# })
 
-Recipe.create({
-  user_id: 1,
-  f2f_url: "http://food2fork.com/view/15f567",
-  title: "Leg of lamb cooked with potatoes",
-  recipe_id: "15f567",
-  image_url: "http://static.food2fork.com/3940_MEDIUMfa28.jpg",
-  social_rank: 98.43662051709592
-})
+# Recipe.create({
+#   user_id: 1,
+#   f2f_url: "http://food2fork.com/view/15f567",
+#   title: "Leg of lamb cooked with potatoes",
+#   recipe_id: "15f567",
+#   image_url: "http://static.food2fork.com/3940_MEDIUMfa28.jpg",
+#   social_rank: 98.43662051709592
+# })
 
-Recipe.create({
-  user_id: 1,
-  f2f_url: "http://food2fork.com/view/47937",
-  title: "Porcini Mushroom Soup",
-  recipe_id: "47937",
-  image_url: "http://static.food2fork.com/porcini_mushroom_soup_recipe8f1f.jpg",
-  social_rank: 98.37394568023093
-})
+# Recipe.create({
+#   user_id: 1,
+#   f2f_url: "http://food2fork.com/view/47937",
+#   title: "Porcini Mushroom Soup",
+#   recipe_id: "47937",
+#   image_url: "http://static.food2fork.com/porcini_mushroom_soup_recipe8f1f.jpg",
+#   social_rank: 98.37394568023093
+# })
 
-Recipe.create({
-  user_id: 1,
-  f2f_url: "http://food2fork.com/view/39101",
-  title: "Beef and Beer With Yellow Potatoes",
-  recipe_id: "39101",
-  image_url: "http://static.food2fork.com/beefbeerpotatoes_300d66c8e1f.jpg",
-  social_rank: 92.53514124771516
-})
+# Recipe.create({
+#   user_id: 1,
+#   f2f_url: "http://food2fork.com/view/39101",
+#   title: "Beef and Beer With Yellow Potatoes",
+#   recipe_id: "39101",
+#   image_url: "http://static.food2fork.com/beefbeerpotatoes_300d66c8e1f.jpg",
+#   social_rank: 92.53514124771516
+# })
 
-Recipe.create({
-  user_id: 1,
-  f2f_url: "http://food2fork.com/view/48141",
-  title: "Buttermilk Summer Squash Soup",
-  recipe_id: "48141",
-  image_url: "http://static.food2fork.com/buttermilk_squash_soupeaa6.jpg",
-  social_rank: 77.66025944343777
-})
+# Recipe.create({
+#   user_id: 1,
+#   f2f_url: "http://food2fork.com/view/48141",
+#   title: "Buttermilk Summer Squash Soup",
+#   recipe_id: "48141",
+#   image_url: "http://static.food2fork.com/buttermilk_squash_soupeaa6.jpg",
+#   social_rank: 77.66025944343777
+# })
 
 
